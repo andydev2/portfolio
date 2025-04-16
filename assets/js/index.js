033199ccfdbe35d7243f2e1a1773808e6b04414e
Andy Dev <88108527+andydev2@users.noreply.github.com>
2025-04-16 18:29:54 -0500

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,18 @@
-let sec = document.querySelectorAll("section");
-let links = document.querySelectorAll(".nav-link");
+const navLinks = document.querySelectorAll(".links");
+const sections = document.querySelectorAll("section");
 
-window.onscroll = () => {
-  sec.forEach((section) => {
-    let top = window.scrollY;
-    let offset = section.offsetTop - 60;
-    let height = section.offsetHeight;
-    let id = section.getAttribute("id");
-
-    if (top >= offset && top < offset + height) {
-      links.forEach((link) => {
-        link.classList.remove("active");
-        document
-          .querySelector(".nav-link[href*=" + id + "] ")
-          .classList.add("active");
-      });
+let currentSection = "home";
+window.addEventListener("scroll", () => {
+  sections.forEach((sections) => {
+    if (window.scrollY >= sections.offsetTop - sections.clientHeight / 1.5) {
+      currentSection = sections.id;
     }
   });
-};
+
+  navLinks.forEach((navLinks) => {
+    if (navLinks.href.includes(currentSection)) {
+      document.querySelector(".active").classList.remove("active");
+      navLinks.classList.add("active");
+    }
+  });
+});
