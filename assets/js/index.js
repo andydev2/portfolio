500d5282c4f3f096d014ee59d497224d33a49d83
Andy Dev <88108527+andydev2@users.noreply.github.com>
2025-04-15 20:20:54 -0500

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+let sec = document.querySelectorAll("section");
+let links = document.querySelectorAll("header nav ul li a");
+
+window.onscroll = () => {
+  sec.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 60;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      links.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav ul li a[href*=" + id + "] ")
+          .classList.add("active");
+      });
+    }
+  });
+};
